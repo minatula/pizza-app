b253a42b4f33b0bd7a8a1cd3e0e1f14efd61460d
minatula <minatula.shakhbazov@gmail.com>
2020-04-17 22:17:32 +0300

    fontawesome added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
 
     <title>Laravel</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
 <body>
     @yield('content')
