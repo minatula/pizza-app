87f2eccf0bc7fb61942209f47854277316efd2b7
minatula <minatula.shakhbazov@gmail.com>
2020-04-17 19:44:08 +0300

    main layout added

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                Content
+            </div>
+        </div>
+    </div>
+@endsection
