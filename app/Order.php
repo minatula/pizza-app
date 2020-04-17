fde86e22e4c4ce9405f4d54bdaffa6985bd1396e
minatula <minatula.shakhbazov@gmail.com>
2020-04-17 23:30:02 +0300

    Relation between Order and Product models added

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    /**
+     * Products from current order
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function products()
+    {
+        return $this->belongsToMany(Product::class)->withPivot('product_amount');
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * Order for current product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function order()
+    {
+        return $this->belongsToMany(Order::class);
+    }
 }
