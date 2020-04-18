ae23527f35fe0699611f668664faf8eea4c0d829
minatula <minatula.shakhbazov@gmail.com>
2020-04-18 14:00:44 +0300

    Order form layout added

---
+++ resources/views/includes/order_form.blade.php
@@ -0,0 +1,67 @@
+<form class="well form-horizontal" style="max-width: 300px;">
+    <fieldset>
+        <legend><h2><b>Fill the form</b></h2></legend><br />
+
+
+        <div class="form-group">
+            <label class="col-12 control-label">Name</label>
+            <div class="col-12 inputGroupContainer">
+                <div class="input-group pb-4">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                    <input
+                        name="customer_name"
+                        placeholder="Name"
+                        class="form-control"
+                        type="text"
+                    />
+                    <div class="position-absolute fixed-bottom text-danger">
+                        Error
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-12 control-label">Phone number</label>
+            <div class="col-12 inputGroupContainer">
+                <div class="input-group pb-4">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+                    <input
+                        name="customer_phone"
+                        placeholder="Phone number"
+                        class="form-control"
+                        type="text"
+                    />
+                    <div class="position-absolute fixed-bottom text-danger">
+                        Error
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-12 control-label">Address</label>
+            <div class="col-12 inputGroupContainer">
+                <div class="input-group pb-4">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+                    <input
+                        name="customer_address"
+                        placeholder="Address"
+                        class="form-control"
+                        type="text"
+                    />
+                    <div class="position-absolute fixed-bottom text-danger">
+                        Error
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-12"><br />
+                <button class="btn btn-warning">SUBMIT</button>
+            </div>
+        </div>
+
+    </fieldset>
+</form>
--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -6,5 +6,10 @@
                 @include('includes/order_details', ['products' => $order->products])
             </div>
         </div>
+        <div class="row">
+            <div class="col-12">
+                @include('includes/order_form')
+            </div>
+        </div>
     </div>
 @endsection
