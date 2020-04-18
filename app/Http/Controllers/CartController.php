044695ba36eb919b2185f29a6a6edca3d3c032ad
minatula <minatula.shakhbazov@gmail.com>
2020-04-18 10:42:22 +0300

    Order existing in CartController added

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,16 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 
 class CartController extends Controller
 {
     public function index()
     {
+
+        $order = $this->getCurrentOrder();
+
+        if (!$order)
+            return redirect()->back()->with('error', 'Your cart is empty');
+
+        return view('cart', compact('order'));
+    }
+
+
+    /**
+     * Return current Order from session or create new
+     *
+     * @return Order
+     */
+    private function getCurrentOrderOrCreate()
+    {
         $orderId = session('orderId');
-        if (!$orderId)
-            return Redirect::route('home')->with('error', 'Your cart is empty');
-        return view();
+        if (!$orderId) {
+            $order = Order::create();
+            session(['orderId' => $order->id]);
+        } else {
+            $order = Order::find($orderId);
+        }
+
+        return $order;
+    }
+
+    /**
+     * Return current Order from session
+     *
+     * @return mixed
+     */
+    private function getCurrentOrder()
+    {
+        $orderId = session('orderId');
+
+        if ($orderId == null)
+            return null;
+
+        $order = Order::find($orderId);
+        return $order;
     }
 }
