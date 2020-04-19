8f33b7e2b181de55e8648ba23d7b9d741d5a578e
minatula <minatula.shakhbazov@gmail.com>
2020-04-19 22:05:18 +0300

    Footer added to layout

---
+++ resources/views/includes/common/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer bg-dark text-white p-3">
+    <div class="container">
+        <div class="row mb-2">
+            <div class="col-12 text-center">
+                Our company is the first company in the world to start producing pizza of the best quality.
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                2020 © PizzaHunt
+            </div>
+        </div>
+    </div>
+</footer>
