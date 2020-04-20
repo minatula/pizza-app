3d6bbbfa952faffef6ff65d02c6989d211188644
minatula <minatula.shakhbazov@gmail.com>
2020-04-20 11:22:16 +0300

    Currency symbol added to total price in cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -7,7 +7,7 @@
                 @include('includes.cart.items', ['products' => $order->products])
 
                 <div class="d-flex justify-content-start mb-3">
-                    <span class="font-weight-bold d-inline-block mr-2">Total: </span>{{ $order->total }}
+                    <span class="font-weight-bold d-inline-block mr-2">Total: </span>{{ $order->total }} {{$currentCurrency->symbol }}
                 </div>
                 <div class="d-flex justify-content-start">
                     <a href="{{ route('order.index') }}" class="btn btn-dark rounded-pill btn-block w-25 d-flex align-items-center justify-content-center m-0">
