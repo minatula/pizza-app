63a580f031bdbce06c6a7e174b55c0434facd74b
minatula <minatula.shakhbazov@gmail.com>
2020-04-20 12:41:25 +0300

    Setup method added yo HomepageTest

--- tests/Feature/HomepageTestTest.php
+++ tests/Feature/HomepageTestTest.php
@@ -4,9 +4,17 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Artisan;
 
 class HomepageTestTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:fresh');
+        $this->seed();
+    }
+
     /**
      * Homepage response test
      *
