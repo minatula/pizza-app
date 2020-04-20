50dbb105411715f0e5955dabbf13fe4a169f3d69
minatula <minatula.shakhbazov@gmail.com>
2020-04-20 10:37:24 +0300

    Currencies deleted from AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,5 @@ class AppServiceProvider extends ServiceProvider
         if($this->app->environment('production')) {
             URL::forceScheme('https');
         }
-        View::share('currencies', Currency::all());
     }
 }
