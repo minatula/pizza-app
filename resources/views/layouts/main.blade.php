f1e895ec9bdb82f404329cf217536e5fd24dcead
minatula <minatula.shakhbazov@gmail.com>
2020-04-20 10:59:12 +0300

    Title in main layout changed

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>PizzaHunt</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
